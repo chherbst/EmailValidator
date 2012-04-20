4b0ab7a194f49aaf13a9ea0cf23e82d81569319b
Christoph Herbst <chris.p.herbst@googlemail.com>
2012-04-20 13:10:00 +0200

    Initial commit of Eclipse project

---
+++ EclipseProjects/EmailValidator/src/tests/EmailValidatorTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import validators.EmailValidator;
+import validators.EmailValidatorImpl;
+
+public class EmailValidatorTest {
+
+	private EmailValidator emailValidator;
+	
+	@Before
+	public void setUp() throws Exception {
+		emailValidator = new EmailValidatorImpl();
+	}
+
+	@Test
+	public void testValidate() {
+		assertTrue(emailValidator.validate("christoph.herbst@mail.tu-berlin.de"));
+		assertFalse(emailValidator.validate(""));
+		assertFalse(emailValidator.validate(null));
+		assertFalse(emailValidator.validate("christoph.herbstmail.tu-berlin.de"));
+		assertFalse(emailValidator.validate("christoph@herbst@mail.tu-berlin.de"));
+		assertFalse(emailValidator.validate("christoph.herbst.@mail.tu-berlin.de"));
+	}
+
+}
