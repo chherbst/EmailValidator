b8e561062912a68849feebbb749c45371d6c8968
Christoph Herbst <christoph.herbst@mail.tu-berlin.de>
2012-04-24 14:06:31 +0200

    test: fixed asserts

--- EclipseProjects/EmailValidator/src/tests/EmailValidatorTest.java
+++ EclipseProjects/EmailValidator/src/tests/EmailValidatorTest.java
@@ -20,17 +20,15 @@ public class EmailValidatorTest {
 	@Test
 	public void testValidate() {
 		assertTrue(emailValidator.validate("christoph.herbst@mail.tu-berlin.de"));
+		assertTrue(emailValidator.validate("chrherbst@cs.tu-berlin.de"));
 		assertTrue(emailValidator.validate("rb.magnus@mailbox.tu-berlin.de"));
+		assertTrue(emailValidator.validate("thamag@cs.tu-berlin.de"));
 		assertFalse(emailValidator.validate(""));
 		assertFalse(emailValidator.validate(null));
 		assertFalse(emailValidator.validate("christoph.herbstmail.tu-berlin.de"));
 		assertFalse(emailValidator.validate("christoph@herbst@mail.tu-berlin.de"));
 		assertFalse(emailValidator.validate("christoph.herbst.@mail.tu-berlin.de"));
-		assertFalse(emailValidator.validate("chrherbst@cs.tu-berlin.de"));
-		assertFalse(emailValidator.validate("thamag@cs.tu-berlin.de"));
 		assertTrue(emailValidator.validate("ahmadnawid.mz@gmail.com"));
-		
-		
 	}
 
 }
