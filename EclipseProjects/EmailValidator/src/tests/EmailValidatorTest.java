26c942419cc0fa4634391479018cb98cf6474794
Naseri <naseri@Naseri-Ubuntu.(none)>
2012-04-25 16:16:16 +0200

    Added my email address

--- EclipseProjects/EmailValidator/src/tests/EmailValidatorTest.java
+++ EclipseProjects/EmailValidator/src/tests/EmailValidatorTest.java
@@ -33,6 +33,8 @@ public class EmailValidatorTest {
 		assertTrue(emailValidator.validate("ahmadnawid.mz@gmail.com"));
 		assertFalse(emailValidator.validate("wazir@ahmadzai@gmail.com"));
 				assertFalse(emailValidator.validate("asgharzada85@yahoo.com"));
+		assertFalse(emailValidator.validate("naseri@@mailbox.tu-berlin.de"));
+		assertTrue(emailValidator.validate("naseri@mailbox.tu-berlin.de"));
 	}
 
 }
